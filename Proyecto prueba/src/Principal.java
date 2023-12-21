a10160ac34cb5c431eb0c89a525b7901055c2a67
herrerosalex <153748614+herrerosalex@users.noreply.github.com>
2023-12-21 18:19:11 +0100

    Incluyo una forma ingeniosa de irse
    Ciao pescao!

--- Proyecto prueba/src/Principal.java
+++ Proyecto prueba/src/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola wakeneknvi");
 		System.out.println("Agur yogur!");
+		System.out.println("Agur pescau!");
+
 
 	}
 
